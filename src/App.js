340e842bfa204fa4a35f2eade962c23eedb0f41c
barbaraaliverti@gmail.com <barbaraaliverti@gmail.com>
2021-05-30 17:22:44 -0300

    Imported files

--- src/App.js
+++ src/App.js
@@ -1,100 +1,10 @@
 import './App.css';
 import React from 'react';
 
-const projectName = "javascript-react-calculator";
 
-const pad = [
-	{
-		keyCode: 65,
-		keyTrigger: 'A',
-		id: 'clear'		
-	},
-	{
-		keyCode: 61,
-		keyTrigger: '=',
-		id: 'equals'		
-	},
-	{
-		keyCode: 110,
-		keyTrigger: '.',
-		id: 'decimal'		
-	},
-	{
-		keyCode: 106,
-		keyTrigger: 'X',
-		id: 'multiply'
-		
-	},
-	{
-		keyCode: 111,
-		keyTrigger: '/',
-		id: 'divide'
-		
-	},
-	{
-		keyCode: 109,
-		keyTrigger: '-',
-		id: 'subtract'		
-	},
-	{
-		keyCode: 107,
-		keyTrigger: '+',
-		id: 'add'
-		
-	},
-	{
-		keyCode: 96,
-		keyTrigger: 0,
-		id: 'zero'
-	},
-	{
-		keyCode: 97,
-		keyTrigger: 1,
-		id: 'one'
-	},
-	{
-		keyCode: 98,
-		keyTrigger: 2,
-		id: 'two'
-	},
-	{
-		keyCode: 99,
-		keyTrigger: 3,
-		id: 'three'
-	},
-	{
-		keyCode: 100,
-		keyTrigger: 4,
-		id: 'four'
-	},
-	{
-		keyCode: 101,
-		keyTrigger: 5,
-		id: 'five'
-	},
-	{
-		keyCode: 102,
-		keyTrigger: 6,
-		id: 'six'
-	},
-	{
-		keyCode: 103,
-		keyTrigger: 7,
-		id: 'seven'
-	},
-	{
-		keyCode: 104,
-		keyTrigger: 8,
-		id: 'eight'
-	},
-	{
-		keyCode: 105,
-		keyTrigger: 9,
-		id: 'nine'
-	}
-];
 
 class Display extends React.Component {
+	// eslint-disable-next-line no-useless-constructor
 	constructor(props) {
 		super(props);
 	}
